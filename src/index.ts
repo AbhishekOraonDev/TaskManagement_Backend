e4dab35e97ef685381e1c157feb0dfc208d650d6
Abhishek Oraon <abhishekoraon082@gmail.com>
2025-02-15 07:05:05 +0530

    link done

--- src/index.ts
+++ src/index.ts
@@ -34,13 +34,16 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }));
+    exposedHeaders: ["set-cookie"], // Allow frontend to access cookies
+  })
+);
+
 
 app.use(express.json());
 app.use(cookieParser());
